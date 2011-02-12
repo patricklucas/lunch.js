a1b780d92ef05b94258edaac2b4e7b3e2d71985a
Patrick Lucas <me@patricklucas.com>
2011-02-12 20:15:16 +0000

    Improve organization in lun.sh

--- lun.sh
+++ lun.sh
@@ -15,12 +15,6 @@ elif [ $1 = "-r" -a -n "$2" ]
 then
     shift
     $CURL $HOST/unnominate.txt -d nomination="$*"
-elif [ $1 = "-d" -a -n "$2" ]
-then
-    $CURL $HOST/drive.txt -d seats="$2"
-elif [ $1 = "--reset" ]
-then
-    $CURL -X POST $HOST/reset.txt
 elif [ $1 = "-u" ]
 then
     $CURL -X POST $HOST/unvote.txt
@@ -28,6 +22,12 @@ elif [ $1 = "-c" -a -n "$2" ]
 then
     shift
     $CURL $HOST/comment.txt -d comment="$*"
+elif [ $1 = "-d" -a -n "$2" ]
+then
+    $CURL $HOST/drive.txt -d seats="$2"
+elif [ $1 = "--reset" ]
+then
+    $CURL -X POST $HOST/reset.txt
 elif [ -n "$1" ]
 then
     $CURL $HOST/vote.txt -d restaurant="$*"
