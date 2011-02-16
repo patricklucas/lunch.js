749548f9918483b8bb7d0093a42461e341ca8aa6
Patrick Lucas <me@patricklucas.com>
2011-02-16 14:39:54 +0000

    Add updating from master to lun.sh

--- lun.sh
+++ lun.sh
@@ -4,6 +4,7 @@ SCRIPT=$( basename "$0" )
 
 CONFIG_DIR="$HOME/.lunch"
 CONFIG_FILE="$CONFIG_DIR/config"
+UPDATE_URL="https://github.com/patricklucas/lunch.js/raw/master/lun.sh"
 
 if [ "$1" = "-h" -a ! -n "$2" ]
 then
@@ -68,6 +69,13 @@ then
     $SCRIPT -C
         Clear your user comment
 EOF
+    exit 0
+fi
+
+if [ "$1" = "--update" -a ! -n "$2" ]
+then
+    curl -s "$UPDATE_URL" > "$0"
+    echo "lun.sh updated successfully."
     exit 0
 fi
 
